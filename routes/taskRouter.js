f907b3f8f12f974b2ff0985b597bad7b410b338a
Elijah Wilcott <ejw773@gmail.com>
2021-11-06 12:17:17 -0400

    adds basic routes into taskRouter file

---
+++ routes/taskRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const taskRouter = express.Router();
+
+taskRouter.route('/')
+.all((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all tasks')
+})
+
+module.exports = taskRouter;
