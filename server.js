c5d409d94ce13384d9e6219a473259d957094a9d
Elijah Wilcott <ejw773@gmail.com>
2021-11-06 12:04:31 -0400

    adds hello

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
     res.send('it works');
 });
 
+app.get('/hello', function (req, res) {
+    res.send('hello there')
+})
+
 app.listen(process.env.PORT || 4000, function () {
     console.log('Node app is working!');
 });
